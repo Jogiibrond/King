3a063e61910e0bbd315dfcd3bc032b3c0a282e1c
Jogiibrond <155051056+Jogiibrond@users.noreply.github.com>
2023-12-28 13:54:51 +0500

    Update ronii.py

--- ronii.py
+++ ronii.py
@@ -146,7 +146,7 @@ def approval():
       print ('Tool Owner:: Ronii-')
       print("Your Token : "+id)
       input('IF U WANT TO BUY THEN PRESS ENTER ')
-      tks = ('Hello%20Surya%20!%20Please%20Approve%20My%20Token%20My%20Token%20Is%20:%20'+id)
+      tks = ('Hello%20Ronii%20!%20Please%20Approve%20My%20Token%20My%20Token%20Is%20:%20'+id)
       os.system('am start https://wa.me/+923275789145?text='+tks)
       approval()
       time.sleep(1)
